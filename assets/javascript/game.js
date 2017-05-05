85e52672dee27cf7224f3b95acdef8f95c9f2b79
Brian K <karabinchak.brian@gmail.com>
2017-05-05 11:10:27 -0400

    Create wins/losses for players, pseudocoded for JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,21 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+/*
+  Initial Start:
+    2 people go on website and enter name
+      First player to enter name -> becomes player 1
+      Second Player enter name -> becomes player 2
+    Player 1 & 2 click on their choice
+      Compare Player 1 and 2 choices
+        Click on image with event listenter
+          Compare choices
+            Increment wins/losses apprropriately
+    If player X leaves - 
+      changes player X wins/losses back to 0, preserve other players info
+    Event listener for chat - 
+      Upon enter - see who typed, enter into database?
+      If disconnect - create disconnect message
+*/
