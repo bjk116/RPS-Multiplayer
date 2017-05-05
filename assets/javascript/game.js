8f5e4083141ec96aa9e8096cf62ec5b8dd1644ea
Brian K <karabinchak.brian@gmail.com>
2017-05-05 10:14:19 -0400

    Initial file commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD-4VhCIktx3Of8cZ4XUgpvUqM7Qx5QaNc",
+  authDomain: "rps-multiplayer-8b1d7.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-8b1d7.firebaseio.com",
+  projectId: "rps-multiplayer-8b1d7",
+  storageBucket: "rps-multiplayer-8b1d7.appspot.com",
+  messagingSenderId: "759752311401"
+};
+firebase.initializeApp(config);
+
